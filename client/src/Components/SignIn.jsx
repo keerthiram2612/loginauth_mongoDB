146e209e1d6d825a1f07570e05935c381d793cd4
Keerthana <keerthiram2612@gmail.com>
2024-06-28 20:48:08 +0530

    updating the signin page

--- client/src/Components/SignIn.jsx
+++ client/src/Components/SignIn.jsx
@@ -4,12 +4,11 @@ import Nature from "../assets/nature.jpeg"
 export default function SignIn() {
   return (
     <div className='signPage'>
-      <div className='images'>
-         
+      <div className='images'></div>
          <div className='components'>
            <label>Email</label><input type="text" placeholder='Enter the username'/><br/>
-           <label>Password</label><input type="text" placeholder='Enter your password'/>
-           </div>
+           <label>Password</label><input type="text" placeholder='Enter your password'/><br/>
+           <button>Login</button>
          </div>
          </div>
   )
